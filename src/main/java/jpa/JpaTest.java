2d5c55ded6cef90f254c6150503d8089c6a0e4a8
jcochet <juliencochet35@gmail.com>
2021-09-10 16:52:44 +0200

    génération de rendez-vous

--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -1,7 +1,15 @@
 package jpa;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+
+import domain.Appointment;
+import domain.User;
+import domain.Worker;
 
 public class JpaTest {
 
@@ -14,19 +22,40 @@ public class JpaTest {
 		EntityTransaction tx = manager.getTransaction();
 		tx.begin();
 
-
 		try {
-
+			createAppointments(manager);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		tx.commit();
-
-
+		
+		listAppointments(manager);
+		
 		manager.close();
 		EntityManagerHelper.closeEntityManagerFactory();
-		//		factory.close();
+		// factory.close();
 	}
 
+	private static void createAppointments(EntityManager manager) {
+		int numOfAppointments = manager.createQuery("Select a From Appointment", Appointment.class).getResultList().size();
+		if(numOfAppointments == 0) {
+			User jcochet = new User("Julien Cochet", "julien.cochet@etudiant.univ-rennes1.fr", "password", new ArrayList<>());
+			manager.persist(jcochet);
+			Worker sababou = new Worker("Sarah Ababou", "sarah.ababou@etudiant.univ-rennes1.fr", "password", new ArrayList<>(), "Développeuse logicielle");
+			manager.persist(sababou);
+			Appointment app1 = new Appointment(new Date(0), 45, jcochet, sababou, "Entretien de 45 minutes");
+			manager.persist(app1);
+			Appointment app2 = new Appointment(new Date(2000000), 45, jcochet, sababou, "Second entretien de 45 minutes");
+			manager.persist(app2);
+		}
+	}
+	
+	private static void listAppointments(EntityManager manager) {
+        List<Appointment> resultList = manager.createQuery("Select a From Appointment a", Appointment.class).getResultList();
+        System.out.println("num of appointments:" + resultList.size());
+        for (Appointment next : resultList) {
+            System.out.println("next appointment: " + next);
+        }
+    }
 
 }
