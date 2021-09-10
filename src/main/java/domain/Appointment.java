538da37dc1b9c135524dde8c0b7e349b10127093
jcochet <juliencochet35@gmail.com>
2021-09-10 18:00:45 +0200

    Ajout d'une requête nommée

--- src/main/java/domain/Appointment.java
+++ src/main/java/domain/Appointment.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+
+@NamedQuery(name = "getAppointments", query = "Select a From Appointment a")
 
 @Entity
 public class Appointment {
--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -28,34 +28,38 @@ public class JpaTest {
 			e.printStackTrace();
 		}
 		tx.commit();
-		
+
 		listAppointments(manager);
-		
+
 		manager.close();
 		EntityManagerHelper.closeEntityManagerFactory();
 		// factory.close();
 	}
 
 	private static void createAppointments(EntityManager manager) {
-		int numOfAppointments = manager.createQuery("Select a From Appointment a", Appointment.class).getResultList().size();
-		if(numOfAppointments == 0) {
-			User jcochet = new User("Julien Cochet", "julien.cochet@etudiant.univ-rennes1.fr", "password", new ArrayList<>());
+		int numOfAppointments = manager.createQuery("Select a From Appointment a", Appointment.class).getResultList()
+				.size();
+		if (numOfAppointments == 0) {
+			User jcochet = new User("Julien Cochet", "julien.cochet@etudiant.univ-rennes1.fr", "password",
+					new ArrayList<>());
 			manager.persist(jcochet);
-			Worker sababou = new Worker("Sarah Ababou", "sarah.ababou@etudiant.univ-rennes1.fr", "password", new ArrayList<>(), "Développeuse logicielle");
+			Worker sababou = new Worker("Sarah Ababou", "sarah.ababou@etudiant.univ-rennes1.fr", "password",
+					new ArrayList<>(), "Développeuse logicielle");
 			manager.persist(sababou);
 			Appointment app1 = new Appointment(new Date(0), 45, jcochet, sababou, "Entretien de 45 minutes");
 			manager.persist(app1);
-			Appointment app2 = new Appointment(new Date(100000000), 45, jcochet, sababou, "Second entretien de 45 minutes");
+			Appointment app2 = new Appointment(new Date(100000000), 45, jcochet, sababou,
+					"Second entretien de 45 minutes");
 			manager.persist(app2);
 		}
 	}
-	
+
 	private static void listAppointments(EntityManager manager) {
-        List<Appointment> resultList = manager.createQuery("Select a From Appointment a", Appointment.class).getResultList();
-        System.out.println("num of appointments:" + resultList.size());
-        for (Appointment next : resultList) {
-            System.out.println("next appointment: " + next);
-        }
-    }
+		List<Appointment> resultList = manager.createNamedQuery("getAppointments", Appointment.class).getResultList();
+		System.out.println("num of appointments:" + resultList.size());
+		for (Appointment next : resultList) {
+			System.out.println("next appointment: " + next);
+		}
+	}
 
 }
