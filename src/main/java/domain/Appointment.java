652f625309deb3154b8253fa0c042f5577f4fb31
jcochet <juliencochet35@gmail.com>
2021-09-10 15:28:51 +0200

    Ajout d'attributs à la classe Appointment

---
+++ src/main/java/domain/Appointment.java
@@ -0,0 +1,74 @@
+package domain;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Appointment {
+
+	private Long id;
+	private Date date;
+	private int duration;
+	private User user;
+	private Worker worker;
+	private String description;
+
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Worker getWorker() {
+		return worker;
+	}
+
+	public void setWorker(Worker worker) {
+		this.worker = worker;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Appointment [id=" + id + ", date=" + date + ", duration=" + duration + ", user=" + user + ", worker="
+				+ worker + ", description=" + description + "]";
+	}
+}
