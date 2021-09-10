241a6681147a93ba8a471b2a193ee50c51ffb470
Sarah-Ab <62647416+Sarah-Ab@users.noreply.github.com>
2021-09-10 15:35:57 +0200

    ajout d'attributs dans les classes User Worker

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,4 +1,41 @@
 package domain;
 
 public class User {
+    private String name;
+    private String email;
+    private String pwd;
+    private Appointnent appointment;
+    public User(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public Appointnent getAppointment() {
+        return appointment;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public void setAppointment(Appointnent appointment) {
+        this.appointment = appointment;
+    }
 }
--- src/main/java/domain/Worker.java
+++ src/main/java/domain/Worker.java
@@ -1,4 +1,25 @@
 package domain;
 
-public class Worker {
+public class Worker extends User {
+
+    private String job;
+    private int id;
+
+    public Worker(){}
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
