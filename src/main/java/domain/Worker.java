5fc1880a0a3e87dd591b4d1a11bbf9b47d0022f4
jcochet <juliencochet35@gmail.com>
2021-09-10 15:55:54 +0200

    ajout d'un nouveau constructeur pour worker

--- src/main/java/domain/Worker.java
+++ src/main/java/domain/Worker.java
@@ -1,37 +1,43 @@
 package domain;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class Worker extends User {
 
-    private String job;
-    private long id;
+	private long id;
+	private String job;
 
-    public Worker(){}
+	public Worker() {
+	}
+	
+	public Worker(String job) {
+		this.job = job;
+	}
 
+	@Id
+	@GeneratedValue
+	public long getId() {
+		return id;
+	}
 
-    public String getJob() {
-        return job;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public void setJob(String job) {
-        this.job = job;
-    }
-    @Id
-    @GeneratedValue
-    public long getId() {
-        return id;
-    }
+	public String getJob() {
+		return job;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
-    @Override
-    public String toString() {
-        return "Worker{" +
-                "job='" + job + '\'' +
-                ", id=" + id +
-                '}';
-    }
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	@Override
+	public String toString() {
+		return "Worker [id=" + id + ", job=" + job + "]";
+	}
+	
 }
